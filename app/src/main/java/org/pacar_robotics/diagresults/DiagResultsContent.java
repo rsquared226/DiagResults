23ac146305830d3fa0d9bd1bb928fe73781ebb18
Rahul Ramkumar <rahulram226@gmail.com>
2017-02-12 15:50:10 -0500

    Create DiagResultItem inside DiagResultsContent
    This class will replace DummyItem inside DummyContent

--- app/src/main/java/org/pacar_robotics/diagresults/DiagResultsContent.java
+++ app/src/main/java/org/pacar_robotics/diagresults/DiagResultsContent.java
@@ -5,4 +5,31 @@ package org.pacar_robotics.diagresults;
  */
 
 public class DiagResultsContent {
+
+	/**
+	 *  Content for each item taken from DiagResults.xml
+	 */
+	public static class DiagResultItem {
+		public final String id;
+		public final String name;
+		public final String shortDesc;
+		public final String longDesc;
+		public final String result;
+		public final String resultMessage;
+		public final String resultSeverity;
+		public final String recommendation;
+
+		public DiagResultItem(String id, String name, String shortDesc, String longDesc,
+		                      String result, String resultMessage, String resultSeverity,
+		                      String recommendation) {
+			this.id = id;
+			this.name = name;
+			this.shortDesc = shortDesc;
+			this.longDesc = longDesc;
+			this.result = result;
+			this.resultMessage = resultMessage;
+			this.resultSeverity = resultSeverity;
+			this.recommendation = recommendation;
+		}
+	}
 }
