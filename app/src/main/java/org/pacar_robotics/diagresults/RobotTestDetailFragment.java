96b21d4a3acf93eff2a9c124f0892ece5c6c96b9
Rahul Ramkumar <rahulram226@gmail.com>
2017-02-12 15:05:08 -0500

    Fix Formatting

--- app/src/main/java/org/pacar_robotics/diagresults/RobotTestDetailFragment.java
+++ app/src/main/java/org/pacar_robotics/diagresults/RobotTestDetailFragment.java
@@ -1,13 +1,14 @@
 package org.pacar_robotics.diagresults;
 
 import android.app.Activity;
-import android.support.design.widget.CollapsingToolbarLayout;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
 import org.pacar_robotics.diagresults.dummy.DummyContent;
 
 /**
@@ -17,52 +18,52 @@ import org.pacar_robotics.diagresults.dummy.DummyContent;
  * on handsets.
  */
 public class RobotTestDetailFragment extends Fragment {
-    /**
-     * The fragment argument representing the item ID that this fragment
-     * represents.
-     */
-    public static final String ARG_ITEM_ID = "item_id";
+	/**
+	 * The fragment argument representing the item ID that this fragment
+	 * represents.
+	 */
+	public static final String ARG_ITEM_ID = "item_id";
 
-    /**
-     * The dummy content this fragment is presenting.
-     */
-    private DummyContent.DummyItem mItem;
+	/**
+	 * The dummy content this fragment is presenting.
+	 */
+	private DummyContent.DummyItem mItem;
 
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
-    public RobotTestDetailFragment() {
-    }
+	/**
+	 * Mandatory empty constructor for the fragment manager to instantiate the
+	 * fragment (e.g. upon screen orientation changes).
+	 */
+	public RobotTestDetailFragment() {
+	}
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        if (getArguments().containsKey(ARG_ITEM_ID)) {
-            // Load the dummy content specified by the fragment
-            // arguments. In a real-world scenario, use a Loader
-            // to load content from a content provider.
-            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+		if (getArguments().containsKey(ARG_ITEM_ID)) {
+			// Load the dummy content specified by the fragment
+			// arguments. In a real-world scenario, use a Loader
+			// to load content from a content provider.
+			mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
 
-            Activity activity = this.getActivity();
-            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
-            if (appBarLayout != null) {
-                appBarLayout.setTitle(mItem.content);
-            }
-        }
-    }
+			Activity activity = this.getActivity();
+			CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+			if (appBarLayout != null) {
+				appBarLayout.setTitle(mItem.content);
+			}
+		}
+	}
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.robottest_detail, container, false);
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	                         Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.robottest_detail, container, false);
 
-        // Show the dummy content as text in a TextView.
-        if (mItem != null) {
-            ((TextView) rootView.findViewById(R.id.robottest_detail)).setText(mItem.details);
-        }
+		// Show the dummy content as text in a TextView.
+		if (mItem != null) {
+			((TextView) rootView.findViewById(R.id.robottest_detail)).setText(mItem.details);
+		}
 
-        return rootView;
-    }
+		return rootView;
+	}
 }
--- app/src/main/java/org/pacar_robotics/diagresults/RobotTestListActivity.java
+++ app/src/main/java/org/pacar_robotics/diagresults/RobotTestListActivity.java
@@ -4,16 +4,15 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 
 import org.pacar_robotics.diagresults.dummy.DummyContent;
 
@@ -29,113 +28,113 @@ import java.util.List;
  */
 public class RobotTestListActivity extends AppCompatActivity {
 
-    /**
-     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
-     * device.
-     */
-    private boolean mTwoPane;
+	/**
+	 * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+	 * device.
+	 */
+	private boolean mTwoPane;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_robottest_list);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_robottest_list);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        toolbar.setTitle(getTitle());
+		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+		toolbar.setTitle(getTitle());
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+		fab.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+						.setAction("Action", null).show();
+			}
+		});
 
-        View recyclerView = findViewById(R.id.robottest_list);
-        assert recyclerView != null;
-        setupRecyclerView((RecyclerView) recyclerView);
+		View recyclerView = findViewById(R.id.robottest_list);
+		assert recyclerView != null;
+		setupRecyclerView((RecyclerView) recyclerView);
 
-        if (findViewById(R.id.robottest_detail_container) != null) {
-            // The detail container view will be present only in the
-            // large-screen layouts (res/values-w900dp).
-            // If this view is present, then the
-            // activity should be in two-pane mode.
-            mTwoPane = true;
-        }
-    }
+		if (findViewById(R.id.robottest_detail_container) != null) {
+			// The detail container view will be present only in the
+			// large-screen layouts (res/values-w900dp).
+			// If this view is present, then the
+			// activity should be in two-pane mode.
+			mTwoPane = true;
+		}
+	}
 
-    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
-        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(DummyContent.ITEMS));
-    }
+	private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+		recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(DummyContent.ITEMS));
+	}
 
-    public class SimpleItemRecyclerViewAdapter
-            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
+	public class SimpleItemRecyclerViewAdapter
+			extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
 
-        private final List<DummyContent.DummyItem> mValues;
+		private final List<DummyContent.DummyItem> mValues;
 
-        public SimpleItemRecyclerViewAdapter(List<DummyContent.DummyItem> items) {
-            mValues = items;
-        }
+		public SimpleItemRecyclerViewAdapter(List<DummyContent.DummyItem> items) {
+			mValues = items;
+		}
 
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.robottest_list_content, parent, false);
-            return new ViewHolder(view);
-        }
+		@Override
+		public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+			View view = LayoutInflater.from(parent.getContext())
+					.inflate(R.layout.robottest_list_content, parent, false);
+			return new ViewHolder(view);
+		}
 
-        @Override
-        public void onBindViewHolder(final ViewHolder holder, int position) {
-            holder.mItem = mValues.get(position);
-            holder.mIdView.setText(mValues.get(position).id);
-            holder.mContentView.setText(mValues.get(position).content);
+		@Override
+		public void onBindViewHolder(final ViewHolder holder, int position) {
+			holder.mItem = mValues.get(position);
+			holder.mIdView.setText(mValues.get(position).id);
+			holder.mContentView.setText(mValues.get(position).content);
 
-            holder.mView.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    if (mTwoPane) {
-                        Bundle arguments = new Bundle();
-                        arguments.putString(RobotTestDetailFragment.ARG_ITEM_ID, holder.mItem.id);
-                        RobotTestDetailFragment fragment = new RobotTestDetailFragment();
-                        fragment.setArguments(arguments);
-                        getSupportFragmentManager().beginTransaction()
-                                .replace(R.id.robottest_detail_container, fragment)
-                                .commit();
-                    } else {
-                        Context context = v.getContext();
-                        Intent intent = new Intent(context, RobotTestDetailActivity.class);
-                        intent.putExtra(RobotTestDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+			holder.mView.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					if (mTwoPane) {
+						Bundle arguments = new Bundle();
+						arguments.putString(RobotTestDetailFragment.ARG_ITEM_ID, holder.mItem.id);
+						RobotTestDetailFragment fragment = new RobotTestDetailFragment();
+						fragment.setArguments(arguments);
+						getSupportFragmentManager().beginTransaction()
+								.replace(R.id.robottest_detail_container, fragment)
+								.commit();
+					} else {
+						Context context = v.getContext();
+						Intent intent = new Intent(context, RobotTestDetailActivity.class);
+						intent.putExtra(RobotTestDetailFragment.ARG_ITEM_ID, holder.mItem.id);
 
-                        context.startActivity(intent);
-                    }
-                }
-            });
-        }
+						context.startActivity(intent);
+					}
+				}
+			});
+		}
 
-        @Override
-        public int getItemCount() {
-            return mValues.size();
-        }
+		@Override
+		public int getItemCount() {
+			return mValues.size();
+		}
 
-        public class ViewHolder extends RecyclerView.ViewHolder {
-            public final View mView;
-            public final TextView mIdView;
-            public final TextView mContentView;
-            public DummyContent.DummyItem mItem;
+		public class ViewHolder extends RecyclerView.ViewHolder {
+			public final View mView;
+			public final TextView mIdView;
+			public final TextView mContentView;
+			public DummyContent.DummyItem mItem;
 
-            public ViewHolder(View view) {
-                super(view);
-                mView = view;
-                mIdView = (TextView) view.findViewById(R.id.id);
-                mContentView = (TextView) view.findViewById(R.id.content);
-            }
+			public ViewHolder(View view) {
+				super(view);
+				mView = view;
+				mIdView = (TextView) view.findViewById(R.id.id);
+				mContentView = (TextView) view.findViewById(R.id.content);
+			}
 
-            @Override
-            public String toString() {
-                return super.toString() + " '" + mContentView.getText() + "'";
-            }
-        }
-    }
+			@Override
+			public String toString() {
+				return super.toString() + " '" + mContentView.getText() + "'";
+			}
+		}
+	}
 }
